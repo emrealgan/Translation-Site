6997028023e56ae7b163b751040e38aa1fc46bd6
emrealgan <alganemre.8@gmail.com>
2024-06-15 11:17:38 +0300

    added register

---
+++ app/api/auth/forgetPass/page.jsx
@@ -0,0 +1,42 @@
+"use client"
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+export default function Login() {
+  const [eMail, setEMail] = useState('');
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700">E-Mail</label>
+          <input
+            type="text"
+            value={eMail}
+            onChange={(e) => setEMail(e.target.value)}
+            className="w-full p-2 border"
+            required
+          />
+        </div>
+        
+        {error && <div className="text-red-500">{error}</div>}
+        <button
+          type="submit"
+          className={"mt-2 p-2 bg-blue-500 text-white"}
+        >
+          Register with Credentials
+        </button>
+      </form>
+    </div>
+  );
+}
--- app/api/auth/register/page.jsx
+++ app/api/auth/register/page.jsx
@@ -2,34 +2,39 @@
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import Link from 'next/link';
 
 export default function Login() {
   const [eMail, setEMail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState(''); // New state for confirmation password
   const [error, setError] = useState('');
-  const router = useRouter()
+  const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const result = await signIn('credentials', {
-      eMail: eMail,
-      password: password,
-      redirect: false,
-    });
 
-    if (result.error) {
-      setError("Hatalı kullanıcı adı veya şifre");
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
     }
-    else if(result.ok){
-      router.push('/');
-      router.refresh();
-    }
+
+  //   const result = await signIn('credentials', {
+  //     eMail: eMail,
+  //     password: password,
+  //     redirect: false,
+  //   });
+
+  //   if (result.error) {
+  //     setError("Invalid email or password");
+  //   } else if (result.ok) {
+  //     router.push('/');
+  //     router.refresh();
+  //   }
   };
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">Login</h1>
+      <h1 className="text-2xl font-bold">Sign Up</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="block text-gray-700">E-Mail</label>
@@ -51,25 +56,25 @@ export default function Login() {
             required
           />
         </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Confirm Password</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="w-full p-2 border"
+            required
+          />
+        </div>
         {error && <div className="text-red-500">{error}</div>}
         <button
           type="submit"
-          className="mt-2 p-2 bg-blue-500 text-white"
+          className={`mt-2 p-2 bg-blue-500 text-white ${password !== confirmPassword ? 'opacity-50 cursor-not-allowed' : ''}`}
+          disabled={password !== confirmPassword} 
         >
-          Login with Credentials
+          Register with Credentials
         </button>
       </form>
-      <div>
-   
-      <Link href="../auth/register">
-        Sign Up
-      </Link>
-      <br/>
-      <Link href="../auth/forgetPass">
-          Forget Password
-      </Link>
-
-      </div>
     </div>
   );
 }
