8693e6be4ff67caf326c45e92ec75eacfbdc09fd
Emre Algan <alganemre.8@gmail.com>
2024-07-09 15:18:23 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
 const nextConfig = {
   env: {
     NEXTAUTH_URL:'http://localhost:3000',
-    NEXTAUTH_SECRET:'yuH03ZyQ3oktpWFVYEzPX7oEPkSYxb5p0sVkQ3BP+FU',
-    GPT4_API_KEY:'sk-proj-3wH94mVlogvzbsT7xjwLT3BlbkFJLTODiVLrtW8n0iaVZSSj',
-    MONGO_URL: 'mongodb+srv://admin:Cny6ZkfEPOXLZm37@mutercim.s7mccpu.mongodb.net/',
-    GOOGLE_CLIENT_ID: '1030311588922-209kdgqr0t0qbglc6kram9r8o3i51fp1.apps.googleusercontent.com',
-    GOOGLE_CLIENT_SECRET: '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',
-    MAILJET_API_KEY: '2f9d5a9559a5952254dbcd3fecab2870',
-    MAILJET_API_SECRET: '8e145f20cf34e746f5e75a355544ecf4'
+    NEXTAUTH_SECRET:'**********',
+    GPT4_API_KEY:'**********',
+    MONGO_URL: '**********',
+    GOOGLE_CLIENT_ID: '**********',
+    GOOGLE_CLIENT_SECRET: '**********',
+    MAILJET_API_KEY: '**********',
+    MAILJET_API_SECRET: '**********'
   }
 };
 
