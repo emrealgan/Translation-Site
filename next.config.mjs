9541ec3439347025efc6cdb0030446203333c30a
Emre Algan <alganemre.8@gmail.com>
2024-07-09 17:01:25 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
 const nextConfig = {
   env: {
     NEXTAUTH_URL:'http://localhost:3000',
-    NEXTAUTH_SECRET:'**********',
-    GPT4_API_KEY:'**********',
-    MONGO_URL: '**********',
-    GOOGLE_CLIENT_ID: '**********',
-    GOOGLE_CLIENT_SECRET: '**********',
-    MAILJET_API_KEY: '**********',
-    MAILJET_API_SECRET: '**********'
+    NEXTAUTH_SECRET:'',
+    GPT4_API_KEY:'',
+    MONGO_URL: '',
+    GOOGLE_CLIENT_ID: '',
+    GOOGLE_CLIENT_SECRET: '',
+    MAILJET_API_KEY: '',
+    MAILJET_API_SECRET: ''
   }
 };
 
