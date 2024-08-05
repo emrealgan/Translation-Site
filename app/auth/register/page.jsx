88b37630343bf12c702c50e7028644a964aaf8ed
Emre Algan <alganemre.8@gmail.com>
2024-08-05 11:38:07 +0300

    Update page.jsx

--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,16 +1,17 @@
-"use client"
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import SixInputsForm from '@/Components/SixInputsForm';
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import SixInputsForm from "@/Components/SixInputsForm";
+import TranslateImage from "@/Components/TranslateImage";
 
 export default function Register() {
-  const [eMail, setEMail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
+  const [eMail, setEMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
   const [passwordValid, setPasswordValid] = useState(false);
   const [showCodeInput, setShowCodeInput] = useState(false);
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
   const router = useRouter();
 
   const handlePasswordChange = (value) => {
@@ -26,10 +27,10 @@ export default function Register() {
     e.preventDefault();
 
     try {
-      const response = await fetch('/api/existMail', {
-        method: 'POST',
+      const response = await fetch("/api/existMail", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           mail: eMail,
@@ -39,10 +40,10 @@ export default function Register() {
       const data = await response.json();
 
       if (response.ok && !data.exists) {
-        const verifyResponse = await fetch('/api/verifyMail', {
-          method: 'POST',
+        const verifyResponse = await fetch("/api/verifyMail", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({
             mail: eMail,
@@ -55,23 +56,23 @@ export default function Register() {
           setCode(verifyData.code);
           setShowCodeInput(true);
         } else {
-          setError('Failed to send verification code');
+          setError("Failed to send verification code");
         }
       } else {
-        setError('Email already exists');
+        setError("Email already exists");
       }
     } catch (error) {
-      setError('An error occurred: ' + error.message);
+      setError("An error occurred: " + error.message);
     }
   };
 
   const handleCodeSubmit = async (isVerified) => {
     if (isVerified) {
       try {
-        const response = await fetch('/api/register', {
-          method: 'POST',
+        const response = await fetch("/api/register", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({
             mail: eMail,
@@ -80,78 +81,81 @@ export default function Register() {
         });
 
         if (response.ok) {
-          console.log('User registered successfully');
-          router.push('/auth/login');
+          console.log("User registered successfully");
+          router.push("/auth/login");
         } else {
-          setError('Failed to register user');
+          setError("Failed to register user");
         }
       } catch (error) {
-        setError('An error occurred: ' + error.message);
+        setError("An error occurred: " + error.message);
       }
     } else {
-      setError('Invalid verification code');
+      setError("Invalid verification code");
     }
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">Sign Up</h1>
-      {!showCodeInput && (
-        <form onSubmit={handleEmailSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700">E-Mail</label>
-            <input
-              type="text"
-              value={eMail}
-              onChange={(e) => setEMail(e.target.value)}
-              className="w-full p-2 border"
-              required
-            />
+    <div className="container flex flex-row p-4 h-screen w-full">
+      <TranslateImage />
+      <div className="w-1/2 h-full px-4 py-32">
+        <h1 className="mb-8 text-2xl font-bold text-blue-300">Sign Up</h1>
+        {!showCodeInput && (
+          <form onSubmit={handleEmailSubmit} className="h-60 w-3/5">
+            <div className="mb-4">
+              <label className="block text-blue-300">E-Mail</label>
+              <input
+                type="text"
+                value={eMail}
+                onChange={(e) => setEMail(e.target.value)}
+                className="w-full p-2 border"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-blue-300">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => handlePasswordChange(e.target.value)}
+                className="w-full p-2 border"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-blue-300">Confirm Password</label>
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="w-full p-2 border"
+              />
+            </div>
+            {!passwordValid && password.length > 0 && (
+              <p className="w-18 h-4 py-2 text-xs">
+                The password must be at least 8 characters and contain at least
+                one uppercase letter.
+              </p>
+            )}
+            {error && <div className="text-gray-900">{error}</div>}
+            <button
+              type="submit"
+              className={`rounded-lg ml-80 mt-8 p-2 bg-blue-300 text-gray-600 hover:bg-blue-200 ${
+                password !== confirmPassword || !passwordValid
+                  ? "opacity-50 cursor-not-allowed"
+                  : ""
+              }`}
+              disabled={password !== confirmPassword || !passwordValid}
+            >
+              Register
+            </button>
+          </form>
+        )}
+        {showCodeInput && (
+          <div>
+            <p>Verification code has been sent to your email.</p>
+            <SixInputsForm code={code} onVerification={handleCodeSubmit} />
+            {error && <p className="text-red-200">{error}</p>}
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => handlePasswordChange(e.target.value)}
-              className="w-full p-2 border"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Confirm Password</label>
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="w-full p-2 border"
-              required
-            />
-          </div>
-          {!passwordValid && password.length > 0 && (
-            <p className="w-18 h-4 py-2 text-s">
-              The password must be at least 8 characters and contain at least one uppercase letter.
-            </p>
-          )}
-          {error && <div className="text-red-500">{error}</div>}
-          <button
-            type="submit"
-            className={`mt-2 p-2 bg-blue-500 text-white ${
-              password !== confirmPassword || !passwordValid ? 'opacity-50 cursor-not-allowed' : ''
-            }`}
-            disabled={password !== confirmPassword || !passwordValid}
-          >
-            Register with Credentials
-          </button>
-        </form>
-      )}
-      {showCodeInput && (
-        <div>
-          <p>Verification code has been sent to your email.</p>
-          <SixInputsForm code={code} onVerification={handleCodeSubmit} />
-          {error && <div className="text-red-500">{error}</div>}
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
